4502a09a5fa35d419aae2315b1437e276a6ef73c
root <root@DESKTOP-OI90J59.localdomain>
2022-11-16 18:38:30 -0500

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -14,5 +17,16 @@ router.get("/api/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
   });
+
+
+  router.post('/api/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
+
+
+
+
+
+
 
 module.exports = router;
