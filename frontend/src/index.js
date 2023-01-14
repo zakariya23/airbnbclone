55471d53f34d8946120f8256b5e136e53f667f3f
root <root@DESKTOP-OI90J59.localdomain>
2023-01-14 18:03:29 -0500

    added Session actions and reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,11 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-
+import * as sessionActions from './store/session';
 
 import configureStore from './store';
+
+
 
 const store = configureStore();
 
@@ -16,6 +18,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 if (process.env.NODE_ENV !== 'production') {
