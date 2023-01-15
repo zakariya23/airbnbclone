8348f522c0cd33eab8761df14a1decaa1c1e2515
root <root@DESKTOP-OI90J59.localdomain>
2023-01-14 19:11:53 -0500

    completed Phase 3: Logout, Navigation parts

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ProfileButton = () => {
+  return (
+    <div style={{ color: "navy", fontSize: "50px" }}>
+      <i class="fa-regular fa-user"></i>
+    </div>
+  );
+};
+
+export default ProfileButton;
